6af8475a0c6efd8b42e2feb2d43618d5238320c5
rayGreycloud <rayGreycloud@gmail.com>
2017-04-13 10:54:08 -0700

    Add server frontpage and facebook webhook

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(`MessBot Server started on port ${port}`);
 });
+
+// Server frontpage
+app.get('/', function (req, res) {
+  res.send('This is MessBot Server ');
+});
+
+// Facebook webhook
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'messbot_verify_token') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Invalid verify token');
+  }
+});
